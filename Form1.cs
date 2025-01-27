e69f6466d7e35cec3b44920f2b7c7dc6529d39b8
Nooblybr <166092409+Nooblybr@users.noreply.github.com>
2025-01-27 14:22:22 -0300

    primeira versão

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+using System.Data;
+using System.Linq.Expressions;
+
+namespace Calculadora_CSharp;
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void PressionarBotao(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+
+        if (button != null)
+        {
+            if (button.Text == "x")
+            {
+                txtVisor.Text += "*";
+            }
+            else
+            {
+                txtVisor.Text += button.Text;
+            }
+        }
+    }
+
+    private void LimparVisor(object sender, EventArgs e)
+    {
+        txtVisor.Clear();
+    }
+
+    private void btnCalcular_Click(object sender, EventArgs e)
+    {
+        string expressao = txtVisor.Text.Replace(",",".");
+        
+        DataTable dt = new DataTable();
+        try{
+            var resultado = Convert.ToDouble(dt.Compute(expressao, null));
+
+            txtVisor.Text = resultado.ToString();
+        }
+        catch (Exception ex) {
+            txtVisor.Text = "ERRO";
+        }
+        
+    }
+}
